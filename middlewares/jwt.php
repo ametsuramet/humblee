9b0d70b23fa05347a99f6ea9cf769c028a5355b1
Ganjar Setia <ganjar_setia@ymail.com>
2017-08-22 12:12:42 +0700

    Make comment easier to read
    Signed-off-by: Ganjar Setia <ganjar_setia@ymail.com>

--- middlewares/jwt.php
+++ middlewares/jwt.php
@@ -19,13 +19,13 @@ $request = new Request;
  * You must specify supported algorithms for your application. See
  * https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-40
  * for a list of spec-compliant algorithms.
-sub Subject - This holds the identifier for the token (defaults to user id)
-iat Issued At - When the token was issued (unix timestamp)
-exp Expiry - The token expiry date (unix timestamp)
-nbf Not Before - The earliest point in time that the token can be used (unix timestamp)
-iss Issuer - The issuer of the token (defaults to the request url)
-jti JWT Id - A unique identifier for the token (md5 of the sub and iat claims)
-aud Audience - The intended audience for the token (not required by default)
+ * sub Subject - This holds the identifier for the token (defaults to user id)
+ * iat Issued At - When the token was issued (unix timestamp)
+ * exp Expiry - The token expiry date (unix timestamp)
+ * nbf Not Before - The earliest point in time that the token can be used (unix timestamp)
+ * iss Issuer - The issuer of the token (defaults to the request url)
+ * jti JWT Id - A unique identifier for the token (md5 of the sub and iat claims)
+ * aud Audience - The intended audience for the token (not required by default)
  */
 
 // $jwt = JWT::encode($token, $key);
