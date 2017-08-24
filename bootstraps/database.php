ca11a29230047fe3a9a2be2ba2b5b744f85d026a
amet suramet <ametsuramet@gmail.com>
2017-08-24 07:53:45 +0700

    simplify db config

--- bootstraps/database.php
+++ bootstraps/database.php
@@ -3,9 +3,10 @@ if (!function_exists('db')) {
 	function db()
 	{
 		global $config;
-		$dsn = 'mysql:host='.$config['database']['db'][$config['app']['APP_ENV']]['host'].';dbname='.$config['database']['db'][$config['app']['APP_ENV']]['database'].';charset=utf8';
-		$usr = $config['database']['db'][$config['app']['APP_ENV']]['user'];
-		$pwd = $config['database']['db'][$config['app']['APP_ENV']]['password'];
+		$db_config = $config['database']['db'][$config['app']['APP_ENV']];
+		$dsn = 'mysql:host='.$db_config['host'].';dbname='.$db_config['database'].';charset=utf8';
+		$usr = $db_config['user'];
+		$pwd = $db_config['password'];
 		return new \Slim\PDO\Database($dsn, $usr, $pwd);
 	}
 }
