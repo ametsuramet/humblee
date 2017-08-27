9708632555b164314143c84be956c10588933593
amet suramet <ametsuramet@gmail.com>
2017-08-27 16:30:23 +0700

    fixing db connection

--- bootstraps/database.php
+++ bootstraps/database.php
@@ -3,7 +3,7 @@ if (!function_exists('db')) {
 	function db()
 	{
 		global $config;
-		$db_config = $config['database']['db'][$config['app']['APP_ENV']];
+		$db_config = $config['database']['db'][env('DB_CONNECTION','development')];
 		$dsn = 'mysql:host='.$db_config['host'].';dbname='.$db_config['database'].';charset=utf8';
 		$usr = $db_config['user'];
 		$pwd = $db_config['password'];
@@ -16,7 +16,7 @@ if (!function_exists('mongo')) {
 	{
 		global $config;
 		// print_r($config['mongo']);
-		$db_config = $config['mongo']['db'][$config['app']['APP_ENV']];
+		$db_config = $config['mongo']['db'][env('MONGO_CONNECTION','development')];
 		$config_str = "";
 		if ($db_config['user'] && $db_config['password']) {
 			$config_str .= "mongodb://".$db_config['user'].":".$db_config['password']."@".$db_config['host'].":".$db_config['port'];
